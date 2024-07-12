f2bbbde9db434176245583adf17cbe51226b7f3f
G-HARSHITH <harshith.22bce20049@vitapstudent.ac.in>
2024-07-12 15:37:57 +0530

    hii

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 
 // mongodb confiq here
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://mern-book-store:harshith2004@cluster0.aqlbzpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://mern-book-store:harshith2004@cluster0.aqlbzpo.mongodb.net/BookInventory?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
